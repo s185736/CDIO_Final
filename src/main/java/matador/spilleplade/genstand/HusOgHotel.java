a9b194722af7de9f18868bb1d821885d8c65a514
noalu <s215793@dtu.dk>
2022-01-09 16:24:52 +0100

    Idé til HuseOgHoteller

---
+++ src/main/java/matador/spilleplade/genstand/HusOgHotel.java
@@ -0,0 +1,35 @@
+package matador.spilleplade.genstand;
+
+import gui_fields.GUI_Street;
+
+public class HusOgHotel {
+    // Funktion for køb af hus
+    // Boolean om spiller ejer hele farvegruppen
+    // Hvis ja ->
+
+// Guide fra Matador GUI Guide
+/*    public static void main(String[] args) {
+
+        // Sætter 2 huse på Rødovrevej
+        ((GUI_Street) gui.getFields()[1]).setHouses(2);
+
+        // Sætter hotel på Hvidovrevej
+        ((GUI_Street) gui.getFields()[3]).setHotel(true);
+
+        // Sætter 3 huse på Roskildevej og fjerner dem igen
+        ((GUI_Street) gui.getFields()[6]).setHouses(3);
+        ((GUI_Street) gui.getFields()[6]).setHouses(0);
+
+        // Sætter 3 huse på Valby Langgade, men ændrer det til et hotel
+        ((GUI_Street) gui.getFields()[8]).setHouses(3);
+        ((GUI_Street) gui.getFields()[8]).setHotel(true);
+
+        // Sætter hotel på Allégade, men fjerner det igen
+        ((GUI_Street) gui.getFields()[9]).setHotel(true);
+        ((GUI_Street) gui.getFields()[9]).setHotel(false);
+    }
+    */
+
+
+
+}
