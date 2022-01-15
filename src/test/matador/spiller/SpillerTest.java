38a0443534aba5a767621540ce20418156fa18c3
MSAC98 <sammy.chauhan27@gmail.com>
2022-01-15 20:47:43 +0100

    Start på test af spiller klassen

---
+++ src/test/matador/spiller/SpillerTest.java
@@ -0,0 +1,24 @@
+package matador.spiller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpillerTest {
+
+    @Test
+    void setIFaengsel() {
+    }
+
+    @Test
+    void tilfoejBalance() {
+    }
+
+    @Test
+    void fjernFaengselsKort() {
+    }
+
+    @Test
+    void erGaaetFallit() {
+    }
+}
