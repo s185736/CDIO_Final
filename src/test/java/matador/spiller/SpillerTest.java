e22fce9d97f4df1b56765fb5d84cd2bb535ec068
s185736 <65380030+s185736@users.noreply.github.com>
2022-01-16 13:53:24 +0100

    Opdatering af SpillerTest.java

--- src/test/java/matador/spiller/SpillerTest.java
+++ src/test/java/matador/spiller/SpillerTest.java
@@ -1,39 +1,44 @@
 package matador.spiller;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-class SpillerTest {
-    //Opretter en test spiller med en konto balance på 5000 kr.
-    Spiller spiller = new Spiller("Test Spiller", 5000);
+public class SpillerTest {
+    private Spiller spiller;
 
+    @Before
+    public void setUp() throws Exception {
+        this.spiller = new Spiller("Test Navn");
+    }
+
+    @After
+    public void rivNed() throws Exception {
+    }
     @Test
-    void tilfoejBalanceTest() {
-        spiller.tilfoejBalance(1000);
-        int forventet = 5000 + 1000;
-        int aktuelle = spiller.getBalance();
-        assertEquals(forventet, aktuelle);
+    public void getNavn() throws Exception {
+        assertTrue(this.spiller.getNavn().equals("Test Navn"));
     }
 
     @Test
-    void trækBalanceTest() {
-        spiller.tilfoejBalance(-1000);
-        int forventet = 5000 - 1000;
-        int aktuelle = spiller.getBalance();
-        assertEquals(forventet, aktuelle);
+    public void getBalance() throws Exception {
+        assertEquals(0, this.spiller.getBalance());
     }
 
     @Test
-    void setIFaengsel() {
+    public void tilfoejBalance() throws Exception {
+        assertEquals(0, this.spiller.getBalance());
+        this.spiller.tilfoejBalance(100);
+        assertEquals(100, this.spiller.getBalance());
     }
 
     @Test
-    void fjernFaengselsKort() {
-    }
-
-    @Test
-    void erGaaetFallit() {
+    public void getSpillerBrik() throws Exception {
+        assertEquals(Spiller.SpillerBrik.BIL1, this.spiller.getType());
+        this.spiller.setType(Spiller.SpillerBrik.BIL6);
+        assertEquals(Spiller.SpillerBrik.BIL6, this.spiller.getType());
     }
 
 }
