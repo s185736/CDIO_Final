da4e99cc9e015a36058a73a318d998c23be37f41
MSAC98 <sammy.chauhan27@gmail.com>
2022-01-16 13:16:34 +0100

    test af terning samt spiller

--- src/test/matador/spilleplade/genstand/TerningTest.java
+++ src/test/matador/spilleplade/genstand/TerningTest.java
@@ -5,9 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TerningTest {
+    Terning terning = new Terning();
 
     @Test
-    void kast() {
+    public void terningKast(){
+        //Der bliver lavet en test på 1000 kast
+        int antalKast = 1000;
+        for(int i = 0; i < antalKast; i++) {
+            //Gemmer resultatet fra metoden kast()
+            terning.kast();
+            int resultat = terning.getFaceValue();
+            //assertTrue vil give fejlmelding hvis resultatet fra metoden kast() ikke er et tal fra 1 til 6.
+            assertTrue(1 <= resultat && resultat <= 6);
+        }
     }
 
     @Test
@@ -17,4 +27,5 @@ class TerningTest {
     @Test
     void getAntalSider() {
     }
+
 }
--- src/test/matador/spiller/SpillerTest.java
+++ src/test/matador/spiller/SpillerTest.java
@@ -5,19 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpillerTest {
-    Spiller spiller;
-
-    @Test
-    void setIFaengsel() {
-    }
+    //Opretter en test spiller med en konto balance på 5000 kr.
+    Spiller spiller = new Spiller("Test Spiller", 5000);
 
     @Test
     void tilfoejBalanceTest() {
-        int temp = spiller.getBalance();
         spiller.tilfoejBalance(1000);
-        int forventet = temp + 1000;
+        int forventet = 5000 + 1000;
         int aktuelle = spiller.getBalance();
         assertEquals(forventet, aktuelle);
+    }
+
+    @Test
+    void trækBalanceTest() {
+        spiller.tilfoejBalance(-1000);
+        int forventet = 5000 - 1000;
+        int aktuelle = spiller.getBalance();
+        assertEquals(forventet, aktuelle);
+    }
+
+    @Test
+    void setIFaengsel() {
     }
 
     @Test
@@ -27,4 +35,5 @@ class SpillerTest {
     @Test
     void erGaaetFallit() {
     }
+
 }
